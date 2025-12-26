9cd5a73c1f80f195d45fe1d3a8e3cbe38c6f8e11
Aqib Ali <aqibali67002@gmail.com>
2025-12-26 08:03:50 -0800

    Ready for Render deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,13 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
